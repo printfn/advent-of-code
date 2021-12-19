f4c748af762f8b947ded1ef58c57f26b2e480dfa
printfn <printfn@users.noreply.github.com>
2021-12-20 10:35:57 +1300

    Remove unused permute function

--- 2021/19.js
+++ 2021/19.js
@@ -10,28 +10,6 @@ function mk2d(a, b) {
     return arr;
 }
 
-// from https://stackoverflow.com/a/20871714
-function permutator(inputArr) {
-  var results = [];
-
-  function permute(arr, memo) {
-    var cur, memo = memo || [];
-
-    for (var i = 0; i < arr.length; i++) {
-      cur = arr.splice(i, 1);
-      if (arr.length === 0) {
-        results.push(memo.concat(cur));
-      }
-      permute(arr.slice(), memo.concat(cur));
-      arr.splice(i, 0, cur[0]);
-    }
-
-    return results;
-  }
-
-  return permute(inputArr);
-}
-
 let scanners = {
 '0': [
 [65,91,116],
