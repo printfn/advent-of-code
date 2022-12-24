7ea49ee9db17ecc796ce124300150d62a151ec6a
printfn <printfn@users.noreply.github.com>
2022-12-24 20:57:43 +0000

    Add day 24

--- snippets.js
+++ snippets.js
@@ -113,63 +113,63 @@ const problem = {
 };
 
 const lowestCostNode = (costs, processed) => {
-  return Object.keys(costs).reduce((lowest, node) => {
-    if (lowest === null || costs[node] < costs[lowest]) {
-      if (!processed.includes(node)) {
-        lowest = node;
-      }
-    }
-    return lowest;
-  }, null);
+    return Object.keys(costs).reduce((lowest, node) => {
+        if (lowest === null || costs[node] < costs[lowest]) {
+            if (!processed.includes(node)) {
+                lowest = node;
+            }
+        }
+        return lowest;
+    }, null);
 };
 
 // function that returns the minimum cost and path to reach Finish
 const dijkstra = (graph) => {
 
-  // track lowest cost to reach each node
-  const costs = Object.assign({finish: Infinity}, graph.start);
+    // track lowest cost to reach each node
+    const costs = Object.assign({finish: Infinity}, graph.start);
 
-  // track paths
-  const parents = {finish: null};
-  for (let child in graph.start) {
-    parents[child] = 'start';
-  }
-
-  // track nodes that have already been processed
-  const processed = [];
-
-  let node = lowestCostNode(costs, processed);
-
-  while (node) {
-    let cost = costs[node];
-    let children = graph[node];
-    for (let n in children) {
-      let newCost = cost + children[n];
-      if (!costs[n]) {
-        costs[n] = newCost;
-        parents[n] = node;
-      }
-      if (costs[n] > newCost) {
-        costs[n] = newCost;
-        parents[n] = node;
-      }
+    // track paths
+    const parents = {finish: null};
+    for (let child in graph.start) {
+        parents[child] = 'start';
     }
-    processed.push(node);
-    node = lowestCostNode(costs, processed);
-  }
 
-  let optimalPath = ['finish'];
-  let parent = parents.finish;
-  while (parent) {
-    optimalPath.push(parent);
-    parent = parents[parent];
-  }
-  optimalPath.reverse();
+    // track nodes that have already been processed
+    const processed = [];
 
-  const results = {
-    distance: costs.finish,
-    path: optimalPath
-  };
+    let node = lowestCostNode(costs, processed);
 
-  return results;
+    while (node) {
+        let cost = costs[node];
+        let children = graph[node];
+        for (let n in children) {
+            let newCost = cost + children[n];
+            if (!costs[n]) {
+                costs[n] = newCost;
+                parents[n] = node;
+            }
+            if (costs[n] > newCost) {
+                costs[n] = newCost;
+                parents[n] = node;
+            }
+        }
+        processed.push(node);
+        node = lowestCostNode(costs, processed);
+    }
+
+    let optimalPath = ['finish'];
+    let parent = parents.finish;
+    while (parent) {
+        optimalPath.push(parent);
+        parent = parents[parent];
+    }
+    optimalPath.reverse();
+
+    const results = {
+        distance: costs.finish,
+        path: optimalPath
+    };
+
+    return results;
 };
